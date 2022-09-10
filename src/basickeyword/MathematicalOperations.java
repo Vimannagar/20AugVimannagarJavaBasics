e490ba57c60952cf10135caa3f9eabd02fbfdf72
velocity.ctc@gmail.com <velocity.ctc@gmail.com>
2022-09-10 10:05:33 +0530

    Static methods

--- src/basickeyword/MathematicalOperations.java
+++ src/basickeyword/MathematicalOperations.java
@@ -18,6 +18,24 @@ public class MathematicalOperations {
 		
 //					+			-				*				/
 //	WAP to perform addition, substraction , multiplication and division of two numbers	
+	
+//		substraction
+		int d = a-b;
+		
+		System.out.println(d);//-40
+		
+//		multiplication
+		
+		int e = a*b;
+		
+		System.out.println(e);//500
+		
+//		division
+		double g = 10;
+		double h = 50;
+		double f = g/h;
+		
+		System.out.println(f);
 		
 		
 	}
---
+++ src/methods/StaticMethod.java
@@ -0,0 +1,36 @@
+package methods;
+
+public class StaticMethod {
+	
+//	access_modifier static void name_of_method()
+//	{
+//		actions to be executed
+//	}
+	
+	public static void additionOfNumbers()
+	{
+		int a =10;
+		int b = 20;
+		int c= a+b;
+		
+		System.out.println("Addtion of "+a+" and "+b+" is "+c);
+	}
+	
+	public static void main(String[] args) {
+		
+		additionOfNumbers();
+		
+		additionOfNumbers();
+		
+		System.out.println("Executed the methods for 2 times");
+		
+		 additionOfNumbers();
+		 
+		 additionOfNumbers();
+		
+	}
+	
+	
+//	WAP to print the output of substraction, multiplication and division of numbers using methods
+
+}
