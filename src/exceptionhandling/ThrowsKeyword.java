16c21ea68bd9113ae4d359dc68bbfa7b5dec63f0
velocity.ctc@gmail.com <velocity.ctc@gmail.com>
2022-11-08 10:13:53 +0530

    Exception

---
+++ src/exceptionhandling/ThrowsKeyword.java
@@ -0,0 +1,23 @@
+package exceptionhandling;
+
+import java.io.FileNotFoundException;
+
+public class ThrowsKeyword {
+	
+	
+	public static void main(String[] args) throws  InterruptedException
+	
+	{
+		
+		System.out.println("first line ");
+		
+		Thread.sleep(5000);
+		
+		System.out.println("Last line ");
+	}
+	
+	
+	
+	
+
+}
---
+++ src/exceptionhandling/ThrowsKeyword2.java
@@ -0,0 +1,18 @@
+package exceptionhandling;
+
+public class ThrowsKeyword2 {
+	
+	public static void main(String[] args) throws NullPointerException 
+	
+	{		
+		String s1 = null;
+		
+		String s2 = "abc";
+		
+		boolean b = s1.equals(s2);
+		
+		System.out.println(b);
+		
+	}
+
+}
