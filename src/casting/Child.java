d2ced4bf7b83c94f021d2ad6431cf4b56ebb645c
velocity.ctc@gmail.com <velocity.ctc@gmail.com>
2022-11-10 10:57:41 +0530

    casting

--- src/casting/Child.java
+++ src/casting/Child.java
@@ -3,20 +3,22 @@ package casting;
 public class Child extends Parent{
 	
 	
-	public static void main(String[] args) {
+		public static void main(String[] args) {
+			
+			Child c = new Child();
+			
+			
+			Parent pp = (Parent)c;// Up casting
+			
 		
-		Child c = new Child();
-		
-		
-		Parent pp = (Parent)c;// Up casting
-		
-		Child ccc = (Child)pp;// Down casting
-		
-		
-//		Parent p = new Parent();
-//		
-//		Child cc = (Child)p; // CastCastException because child reference variable cannot have a parent object
-		
+			Child ccc = (Child)pp;// Down casting
+			
+			
+	//		Parent p = new Parent();
+	//		
+	//		Child cc = (Child)p; // CastCastException because child reference variable cannot have a parent object
+			
+				
 		
 		
 		
